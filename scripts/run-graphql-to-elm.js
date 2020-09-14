f350e971c49d7db0ee25503933f81fe877e9076b
tadityar <tadityar@gmail.com>
2020-09-14 14:35:58 +0800

    ignore /build files

--- scripts/run-graphql-to-elm.js
+++ scripts/run-graphql-to-elm.js
@@ -7,43 +7,13 @@ const mkdirp = require('mkdirp')
 // pre-process graphql files
 const srcPath = 'src'
 const buildPath = 'build'
-// const fragmentSuffix = '.fragment.gql'
-// const fragments = {}
-// mkdirp.sync(buildPath)
-// // 1. build a dictionary of fragmentName -> fragmentContent from *.fragment.gql
-// glob.sync(srcPath + '/Portal2API/*' + fragmentSuffix).forEach((srcFile, i) => {
-//   const fragmentName = path.basename(srcFile).replace(fragmentSuffix, '')
-//   fragments[fragmentName] = fs.readFileSync(srcFile, { encoding: 'utf8' })
-// })
-// 2. for each *.graphql
-//    a. copy into buildPath
-//    b. for each fragmentName found used in graphql file, append fragmentContent
+
+// for each *.graphql, copy into buildPath
 glob.sync(srcPath + '/db/*.graphql').forEach((srcFile, i) => {
   const buildFile = path.join(buildPath, srcFile.substring(srcPath.length))
   mkdirp.sync(path.dirname(buildFile))
   const srcContent = fs.readFileSync(srcFile, { encoding: 'utf8' })
-  // const appendices = []
-  // for (const fragmentName in fragments) {
-  //   if (srcContent.indexOf('...' + fragmentName + '\n') !== -1) {
-  //     appendices.push(fragments[fragmentName])
-  //   }
-  // }
   fs.writeFileSync(buildFile, srcContent, { encoding: 'utf8' })
-
-  // using explicit index count loop because we're dynamically growing `appendices` inside the loop
-  // for (let i = 0; appendices[i]; i++) {
-  //   const fragmentContent = appendices[i]
-  //   for (const fragmentName in fragments) {
-  //     if (fragmentContent.indexOf('...' + fragmentName + '\n') !== -1) {
-  //       // what if fragment contains fragments, add on!
-  //       if (appendices.indexOf(fragments[fragmentName]) === -1) {
-  //         // but only if it's not already added
-  //         appendices.push(fragments[fragmentName])
-  //       }
-  //     }
-  //   }
-  //   fs.appendFileSync(buildFile, '\n\n' + fragmentContent, { encoding: 'utf8' })
-  // }
 })
 
 // generate elm from graphql files
